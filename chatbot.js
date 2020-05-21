bf429bf73b46f9d4ac5de15c6b42babf5f57f301
JeuneMilford <milford.j@maristcollege.school.nz>
2020-05-22 08:48:15 +1200

    Hello...

---
+++ chatbot.js
@@ -0,0 +1,18 @@
+const userInput =document.querySelector("userInput"); 
+
+const submit =document.querySelector("submit"); 
+
+const weightloss =document.querySelector("weightloss"); 
+
+ 
+ 
+
+submit.addEventListener("click", response); 
+
+ 
+ 
+ 
+
+function response() ( 
+
+    weightloss.textContent ="Hello" + userInput.value; 
